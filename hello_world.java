22ede748d478c8d9d4e62c104387824c9dab8176
Alex Konas <alex.konas@menloschool.org>
2020-09-02 15:10:33 -0700

    yes

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,20 @@
 public class hello_world {
 	public static void main (String [] args)
 	{
-	System.out.println("hello world");
+		System.out.println("Number of ways: " + NumberOfWaysToMakeChange(6)); 
 	}
+
+	public static int NumberOfWaysToMakeChange(int cents)
+	{
+		int ways = 1;
+		if (cents <= 0)
+		{
+			return ways;
+		}
+		else
+		{
+			return NumberOfWaysToMakeChange(cents-25) + NumberOfWaysToMakeChange(cents-10) + NumberOfWaysToMakeChange(cents-5) + NumberOfWaysToMakeChange(cents-1);	
+		}
+	}
+
 }
